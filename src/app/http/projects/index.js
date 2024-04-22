ac637e9e1d4e4bc199bbed9c32c28862a0fd5d5a
githiago-f <tfarias@protonmail.com>
2024-04-21 22:49:22 -0300

    create project and user forms

--- src/app/http/projects/index.js
+++ src/app/http/projects/index.js
@@ -1,4 +1,8 @@
 import { Router } from 'express';
+import { render } from '../../../infra/utils/render.js';
 
 const router = Router();
+
+router.get('/', render('create-project', { title: 'Novo projeto'}));
+
 export default router;
