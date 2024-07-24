159cbd8c5047a6bb9aaa6a5e181a3e9d0d2677e9
githiago-f <tfarias@protonmail.com>
2024-07-24 10:24:12 -0300

    update cert paths

--- src/infra/cert.js
+++ src/infra/cert.js
@@ -3,7 +3,7 @@ import path from 'node:path';
 
 export async function getKeyAndCert() {
     return {
-        key: await readFile(process.env.SSL_KEY_PATH ?? path.join(process.cwd(), 'key.pem')),
-        cert: await readFile(process.env.SSL_CERT_PATH ?? path.join(process.cwd(), 'certificate.pem'))
+        key: await readFile(process.env.SSL_KEY_PATH ?? path.join(process.cwd(), '.ssh/key.pem')),
+        cert: await readFile(process.env.SSL_CERT_PATH ?? path.join(process.cwd(), '.ssh/certificate.pem'))
     };
 }
